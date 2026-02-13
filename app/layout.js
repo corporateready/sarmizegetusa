2b18bafd0da0b5430d2387e6d9d63c94f3f5db67
Evgheni Nujnenco <nev.front.dev@gmail.com>
2026-02-13 11:23:47 +0200

    created RU version first fixed

--- app/layout.js
+++ app/layout.js
@@ -20,7 +20,7 @@ export default function RootLayout({ children }) {
       <head>
         <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=5"/>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
         <meta name="robots" content="index, follow" />
         <meta
           name="viewport"
